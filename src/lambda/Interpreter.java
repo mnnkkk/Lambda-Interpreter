9a0db15e83042e07a38d3141470c3d90e7f45583
mnnkkk <46259595+mnnkkk@users.noreply.github.com>
2019-05-19 22:46:28 +0800

    v 2.0

--- src/lambda/Interpreter.java
+++ src/lambda/Interpreter.java
@@ -16,7 +16,7 @@ public class Interpreter {
         if(current != null){
             postOrderSimplize(current.leftChild);
             postOrderSimplize(current.rightChild);
-            //
+            
             if(current.type==NodeType.ABSTRACTION){
                 if(current.leftChild!=null
                         &&current.rightChild!=null
@@ -27,59 +27,70 @@ public class Interpreter {
                 else if(current.leftChild!=null
                         &&current.rightChild!=null){
                     if(current.leftChild.qcontent.length()!=1)
-                        current.leftChild.qcontent=" ("+current.leftChild.qcontent+")";
+                        current.leftChild.qcontent="("+current.leftChild.qcontent+")";
                     if(current.rightChild.qcontent.length()!=1)
                         current.rightChild.qcontent=" ("+current.rightChild.qcontent+")";
                     current.bodyOfAbstraction=current.leftChild.qcontent+current.rightChild.qcontent;
                     current.qcontent="\\"+current.parmOfAbstraction+"."+current.bodyOfAbstraction;
-
-                    //current.bodyOfAbstraction="("+current.leftChild.qcontent+") ("+current.rightChild.qcontent+")";
-                    //current.qcontent="\\"+current.parmOfAbstraction+"."+current.bodyOfAbstraction;
                 }
-                current.leftChild=null;
-                current.rightChild=null;
+
             }
-            //
+            
             else if(current.type==NodeType.APPLICATION){
                 if(current.leftChild!=null
                         &&current.rightChild!=null
                         &&current.leftChild.type==NodeType.ABSTRACTION){
                     if(current.rightChild.qcontent.contains("."))
                         current.rightChild.qcontent='\\'+current.rightChild.qcontent;
-                    if(current.rightChild.qcontent.length()!=1)
+                    if(current.rightChild.qcontent.length()!=1&&current.rightChild.qcontent.charAt(0)=='\\')
                         current.rightChild.qcontent='('+current.rightChild.qcontent+')';
                     current.contentOfApplication=current.leftChild.bodyOfAbstraction.replaceAll(String.valueOf(current.leftChild.parmOfAbstraction),current.rightChild.qcontent);
                     current.qcontent=current.contentOfApplication;
                 }
                 else if(current.leftChild!=null
                         &&current.rightChild!=null){
-                    if(current.leftChild.qcontent.length()!=1)
-                        current.leftChild.qcontent=" ("+current.leftChild.qcontent+")";
-                    if(current.rightChild.qcontent.length()!=1)
+                    if(current.leftChild.qcontent.length()!=1&&current.leftChild.qcontent.charAt(0)=='\\')
+                        current.leftChild.qcontent="("+current.leftChild.qcontent+")";
+                    if(current.rightChild.qcontent.length()!=1&&current.rightChild.qcontent.charAt(0)=='\\')
                         current.rightChild.qcontent=" ("+current.rightChild.qcontent+")";
                     current.contentOfApplication=current.leftChild.qcontent+" "+current.rightChild.qcontent;
                     current.qcontent=current.contentOfApplication;
                 }
-                current.leftChild=null;
-                current.rightChild=null;
             }
+            current.leftChild=null;
+            current.rightChild=null;
         }
     }
 
-    public static void main(String[] args) {
+    public static String toInterpreter(String source){
+        String before=".",current=source;
+        while(!before.equals(current)){
+            before=current;
+            Parser parser = new Parser(current);
+            Node head = parser.parse();
+            cutTwoParaAtom(head);
+            postOrderSimplize(head);
+            current=head.toString();
+            if(current.length()>before.length())
+                break;
+        }
+        return current;
+    }
 
-        //String source = "(\\x.(\\y.y x) z) v";  //ok
-        String source = "(\\x.x (x y)) m";  //ok
-        //String source = "(\\x.y) z";  //ok
-        //String source = "(\\x.x x) (\\x.x x)";  //ok
-        //String source = "(\\x.x x y) (\\x.x x y)";  //ok (add codes to cheak longer length?)
-        //String source = "\\x.\\y.y x a b";  //ok
-        Parser parser = new Parser(source);
-        Node head = parser.parse();
-        Node current=head;
-        Interpreter.cutTwoParaAtom(current);
-        current=head;
-        Interpreter.postOrderSimplize(current);
-        head.printNode();
+    public static void main(String[] args) {
+        /*
+        *input law(codes to ignore this will be made if time permiting)
+        *") (" instead of ")("
+        */
+
+        //Here are examples
+        //String source = "(\\x.(\\y.y x) z) v";  //success
+        //String source = "(\\x.x (x y)) m";  //success
+        //String source = "(\\x.x x) (\\x.x x)";  //success
+        //String source = "(\\x.x x y) (\\x.x x y)";  //ok (this will be longer and longer)
+        //String source="(\\x.\\y.x y) m n";  //success
+        String source="(\\x.\\y.\\z.x z(y z)) (\\x.x) m n";  //success
+
+        System.out.println(toInterpreter(source));
     }
 }
