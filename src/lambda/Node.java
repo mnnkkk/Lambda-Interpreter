336774f63be3c6444068ca38a44889ba443cf8e7
mnnkkk <46259595+mnnkkk@users.noreply.github.com>
2019-05-19 22:47:22 +0800

    v 2.0

--- src/lambda/Node.java
+++ src/lambda/Node.java
@@ -1,4 +1,5 @@
 package lambda;
+import java.util.*;
 
 public class Node{
     public NodeType type=null;
@@ -11,75 +12,6 @@ public class Node{
 
     public Node leftChild=null;
     public Node rightChild=null;
-
-    public NodeType typeOfString(String s){
-        if(s.length()==1)    //a
-            return NodeType.ATOM;
-        if(s.charAt(0)=='\\')   //xxx xxx
-            return NodeType.ABSTRACTION;
-        return NodeType.APPLICATION;
-    }
-
-    public String offLR(String s){
-        if(s.charAt(0)=='('
-                &&s.charAt(s.length()-1)==')'
-                &&!s.contains(") ("))
-            s=s.substring(1, s.length()-1);
-        return s;
-    }
-
-    public void extend(){
-        if(this.type==NodeType.ATOM)
-            return;
-        if(this.type==NodeType.ABSTRACTION){
-            if(typeOfString(this.bodyOfAbstraction)==NodeType.ATOM
-                    ||typeOfString(this.bodyOfAbstraction)==NodeType.ABSTRACTION)
-                return;
-            //typeOfString(this.bodyOfAbstraction)==APPLICATION
-            if(bodyOfAbstraction.charAt(0)!='('){
-                this.leftChild=new Node(bodyOfAbstraction.substring(0,1));
-                this.rightChild=new Node(bodyOfAbstraction.substring(2,bodyOfAbstraction.length()));
-            }
-            else{
-                int leftcount=0,rightcount=0;
-                int i;
-                for(i=0;i<bodyOfAbstraction.length();i++){
-                    if(bodyOfAbstraction.charAt(i)=='(')
-                        leftcount++;
-                    else if(bodyOfAbstraction.charAt(i)==')')
-                        rightcount++;
-                    if(leftcount==rightcount)
-                        break;
-                }
-                this.leftChild=new Node(bodyOfAbstraction.substring(0,i+1));
-                this.rightChild=new Node(bodyOfAbstraction.substring(i+2,bodyOfAbstraction.length()));
-            }
-            this.leftChild.extend();
-            this.rightChild.extend();
-        }
-        if(this.type==NodeType.APPLICATION){
-            if(false&&contentOfApplication.charAt(0)!='('){
-                this.leftChild=new Node(contentOfApplication.substring(0,1));
-                this.rightChild=new Node(contentOfApplication.substring(2,contentOfApplication.length()));
-            }
-            else{
-                int leftcount=0,rightcount=0;
-                int i;
-                for(i=0;i<contentOfApplication.length();i++){
-                    if(contentOfApplication.charAt(i)=='(')
-                        leftcount++;
-                    else if(contentOfApplication.charAt(i)==')')
-                        rightcount++;
-                    if(leftcount==rightcount)
-                        break;
-                }
-                this.leftChild=new Node(contentOfApplication.substring(0,i+1));
-                this.rightChild=new Node(contentOfApplication.substring(i+2,contentOfApplication.length()));
-            }
-            this.leftChild.extend();
-            this.rightChild.extend();
-        }
-    }
 
     Node(String content){
         content=offLR(content);
@@ -95,12 +27,90 @@ public class Node{
         }
     }
 
-    void printNode(){
+    public NodeType typeOfString(String s){
+        if(s.length()==1)    //a
+            return NodeType.ATOM;
+        if(s.charAt(0)=='\\')   // \x.x y
+            return NodeType.ABSTRACTION;
+        return NodeType.APPLICATION;  // s d || (\x.x y) k
+    }
+
+    public boolean isValid(String s) {
+        char[] stack = new char[s.length() + 1];
+        int top = 1;
+        for(char c : s.toCharArray())
+            if(c == '(')
+                stack[top++] = c;
+            else if(c == ')' && stack[--top] != '(')
+                return false;
+        return top == 1;
+    }
+    public String offLR(String s){
+        if(s.charAt(0)=='('&&s.charAt(s.length()-1)==')'){
+            String after=s.substring(1, s.length()-1);
+            return isValid(after)?after:s;
+        }
+        else
+            return s;
+    }
+
+    public void extend(){
         if(this.type==NodeType.ATOM)
-            System.out.println(contentOfAtom);
+            return;
+
+        if(this.type==NodeType.ABSTRACTION){
+            if(typeOfString(this.bodyOfAbstraction)==NodeType.ATOM
+                    ||typeOfString(this.bodyOfAbstraction)==NodeType.ABSTRACTION)
+                return;
+            //typeOfString(this.bodyOfAbstraction)==APPLICATION
+            if(!bodyOfAbstraction.contains("."))
+                return;
+            if(bodyOfAbstraction.charAt(0)!='('){
+                this.leftChild=new Node(bodyOfAbstraction.substring(0,1));
+                this.rightChild=new Node(bodyOfAbstraction.substring(2,bodyOfAbstraction.length()));
+            }
+            else{
+                int leftcount=0,rightcount=0;
+                int i;
+                for(i=bodyOfAbstraction.length()-1;i>=0;i--){
+                    if(bodyOfAbstraction.charAt(i)=='(')
+                        leftcount++;
+                    else if(bodyOfAbstraction.charAt(i)==')')
+                        rightcount++;
+                    if(leftcount==rightcount&&bodyOfAbstraction.charAt(i)==' ')
+                        break;
+                }
+                this.leftChild=new Node(bodyOfAbstraction.substring(0,i));
+                this.rightChild=new Node(bodyOfAbstraction.substring(i+1,bodyOfAbstraction.length()));
+            }
+        }
+        if(this.type==NodeType.APPLICATION){
+            if(!contentOfApplication.contains("."))
+                return;
+            int leftcount=0,rightcount=0;
+            int i;
+            for(i=contentOfApplication.length()-1;i>=0;i--){
+                if(contentOfApplication.charAt(i)=='(')
+                    leftcount++;
+                else if(contentOfApplication.charAt(i)==')')
+                    rightcount++;
+                if(leftcount==rightcount&&contentOfApplication.charAt(i)==' ')
+                    break;
+            }
+                this.leftChild=new Node(contentOfApplication.substring(0,i));
+                this.rightChild=new Node(contentOfApplication.substring(i+1,contentOfApplication.length()));   
+        }
+        this.leftChild.extend();
+        this.rightChild.extend();
+    }
+
+    public String toString(){
+        if(this.type==NodeType.ATOM)
+            return String.valueOf(contentOfAtom);
         if(this.type==NodeType.APPLICATION)
-            System.out.println(contentOfApplication);
+            return contentOfApplication;
         if(this.type==NodeType.ABSTRACTION)
-            System.out.println("\\"+parmOfAbstraction+"."+bodyOfAbstraction);
+            return "\\"+parmOfAbstraction+"."+bodyOfAbstraction;
+        return null;
     }
 }
