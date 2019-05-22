72e5f4445d3c0ada89132c99b0795473fd8ed51f
mnnkkk <46259595+mnnkkk@users.noreply.github.com>
2019-05-22 13:39:29 +0800

    v 2.2

--- src/lambda/Interpreter.java
+++ src/lambda/Interpreter.java
@@ -5,11 +5,28 @@ public class Interpreter {
     public static String replaceStr(String source,char before,String after){
         StringBuffer b = new StringBuffer();
         for(int i=0;i<source.length();i++){
+            if(source.charAt(i)=='\\'){
+                b.append('\\');
+                i++;
+                b.append(source.charAt(i));
+                continue;
+            }
             if(source.charAt(i)==before)
                 b.append(after);
             else
                 b.append(source.charAt(i));
         }
+        return b.toString();
+    }
+
+    public static String transInput(String s){
+        StringBuffer b = new StringBuffer(); 
+        for(int i=0;i<s.length()-1;i++){
+            b.append(s.charAt(i));
+            if(s.charAt(i)==')'&&s.charAt(i+1)!=')'&&s.charAt(i+1)!=' ')
+                b.append(' ');
+        }
+        b.append(s.charAt(s.length()-1));
         return b.toString();
     }
 
@@ -25,52 +42,37 @@ public class Interpreter {
     }
 
     public static void postOrderSimplize(Node current){
-        if(current != null){
+        if(current != null&&current.leftChild!=null&&current.rightChild!=null){
+
             postOrderSimplize(current.leftChild);
             postOrderSimplize(current.rightChild);
             
             if(current.type==NodeType.ABSTRACTION){
-                if(current.leftChild!=null
-                &&current.rightChild!=null
-                &&current.leftChild.type==NodeType.ABSTRACTION){
-                    current.bodyOfAbstraction=replaceStr(current.leftChild.bodyOfAbstraction, current.leftChild.parmOfAbstraction,current.rightChild.qcontent);
+                if(current.leftChild.type==NodeType.ABSTRACTION){
+                    current.bodyOfAbstraction=replaceStr(current.leftChild.bodyOfAbstraction,current.leftChild.parmOfAbstraction,current.rightChild.qcontent);
                     current.qcontent="\\"+current.parmOfAbstraction+"."+current.bodyOfAbstraction;
                 }
-                else if(current.leftChild!=null
-                &&current.rightChild!=null
-                &&current.leftChild.qcontent.charAt(0)=='\\'
-                &&current.leftChild.qcontent.length()>3){
-                    current.bodyOfAbstraction=replaceStr(current.leftChild.qcontent.substring(3,current.leftChild.qcontent.length())
-                                                        ,current.leftChild.qcontent.charAt(1)
-                                                        ,current.rightChild.qcontent);
-                    current.qcontent="\\"+current.parmOfAbstraction+"."+current.bodyOfAbstraction;
-                }
-                else if(current.leftChild!=null
-                &&current.rightChild!=null){
+                else{
                     if(current.leftChild.qcontent.length()!=3||current.leftChild.qcontent.charAt(0)!='\\'){
                         if(current.leftChild.qcontent.length()!=1)
                             current.leftChild.qcontent="("+current.leftChild.qcontent+")";
                         if(current.rightChild.qcontent.length()!=1)
                             current.rightChild.qcontent=" ("+current.rightChild.qcontent+")";
+                        if(current.rightChild.qcontent.length()==1)
+                        current.rightChild.qcontent=" "+current.rightChild.qcontent;
                     }
-                    current.bodyOfAbstraction="("+current.leftChild.qcontent+current.rightChild.qcontent+")";
-                    current.qcontent="\\"+current.parmOfAbstraction+"."+current.bodyOfAbstraction;
+                    current.bodyOfAbstraction=current.leftChild.qcontent+current.rightChild.qcontent;    //////[1]
+                    current.qcontent="\\"+current.parmOfAbstraction+".("+current.bodyOfAbstraction+")";
                 }
-
             }
-            
             else if(current.type==NodeType.APPLICATION){
-                if(current.leftChild!=null
-                &&current.rightChild!=null
-                &&current.leftChild.type==NodeType.ABSTRACTION){
-
+                if(current.leftChild.type==NodeType.ABSTRACTION){
                     if(current.rightChild.qcontent.length()!=1&&current.rightChild.qcontent.charAt(0)=='\\')
                         current.rightChild.qcontent="("+current.rightChild.qcontent+")";
                     current.contentOfApplication=replaceStr(current.leftChild.bodyOfAbstraction,current.leftChild.parmOfAbstraction,current.rightChild.qcontent);
                     current.qcontent=current.contentOfApplication;
                 }
-                else if(current.leftChild!=null
-                        &&current.rightChild!=null){
+                else{
                     if(current.leftChild.qcontent.length()!=1&&current.leftChild.qcontent.charAt(0)=='\\')
                         current.leftChild.qcontent="("+current.leftChild.qcontent+")";
                     if(current.rightChild.qcontent.length()!=1)
@@ -86,7 +88,7 @@ public class Interpreter {
 
     public static String toInterpreter(String source){
         String before=".",current=source;
-        while(!before.equals(current)){
+        do{
             current=transInput(current);
             before=current;
             Parser parser = new Parser(current);
@@ -94,21 +96,9 @@ public class Interpreter {
             cutTwoParaAtom(head);
             postOrderSimplize(head);
             current=head.toString();
-            //if(current.length()>before.length())
-                //break;
-        }
+            //System.out.println(current);
+        }while(!before.equals(current));
         return current;
-    }
-
-    public static String transInput(String s){
-        StringBuffer b = new StringBuffer(); 
-        for(int i=0;i<s.length()-1;i++){
-            b.append(s.charAt(i));
-            if(s.charAt(i)==')'&&s.charAt(i+1)!=')'&&s.charAt(i+1)!=' ')
-                b.append(' ');
-        }
-        b.append(s.charAt(s.length()-1));
-        return b.toString();
     }
     public static void main(String[] args) {
 
@@ -119,29 +109,21 @@ public class Interpreter {
         String AND="(\\p.\\q.p q p)";
         String OR="(\\p.\\q.p p q)";
         String NOT="(\\p.\\a.\\b.p b a)";
-        //Here are examples
+
         //String source = "(\\x.(\\y.y x) z) v";  //succeed
         //String source = "(\\x.x (x y)) m";  //succeed
         //String source = "(\\x.x x) (\\x.x x)";  //succeed
         //String source = "(\\x.x x y) (\\x.x x y)";  //succeed
         //String source="(\\x.\\y.x y) m n";  //succeed
-        //String source="(\\x.\\y.\\z.x z(y z)) (\\x.x) m n";  //succeed
-        //String source="f ((\\f.\\x.f x) f x)";  //succeed
-        //String source="\\x.f ((\\f.\\x.f x) f x)";  //succeed
-        String source=NOT+FALSE;
-        if(true){
-            source=transInput(source);
-            System.out.println(toInterpreter(source));
-        }
-        else{
-            Parser parser = new Parser(source);
-            Node head = parser.parse();
-            cutTwoParaAtom(head);
-            postOrderSimplize(head);
-            //head=head.rightChild;
+        //String source="(\\x.\\y.\\z.x z (y z)) (\\x.x) m n";  //succeed   [1]
+        //String source="\\x.f ((\\f.\\x.f x) f x)";  //succeed   [1]
+        //String source="\\x.f ((\\x.x) x)";  //succeed   [1]
 
-            source=head.toString();
-            System.out.println(source);
-        }
+        //String source=SUCC+"("+SUCC+ZERO+")";   //succeed
+        //String source=NOT+TRUE;  //succeed
+        String source=AND+FALSE+TRUE;   //succeed
+        
+        source=transInput(source);
+        System.out.println(toInterpreter(source));        
     }
 }
